1bdb0f6016d864ae8d61315cc9527a886ea654f3
elaine16 <elaine.adm.redes@gmail.com>
2021-02-24 09:42:39 -0300

    Update EntrevistaEmbaracosa.java

--- Desafios/Praticando programação em Java/EntrevistaEmbaracosa.java
+++ Desafios/Praticando programação em Java/EntrevistaEmbaracosa.java
@@ -51,3 +51,35 @@ a
 */
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+
+public class Entrevista_Embaracosa {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNext()) {
+            String input = scanner.nextLine();
+            Integer limit = (int) Math.floor(input.length() / 2);
+            Integer end = input.length();
+            Integer aux = 0;
+            if(input.length() == 1) {
+                System.out.println(input);
+            } else if (limit >= 1) {
+                for (int i = limit; i >= 1; i--) {
+                    if(input.substring(end-i, end).equals(input.substring(end-(2*i),end-i))) {
+                        aux = i;
+                        System.out.println(input.substring(0, (end - aux)));
+                    } else if (limit == 1) {
+                        System.out.println(input);
+                    }
+                }
+                if(limit>1 && aux == 0) System.out.println(input);
+            }
+        }scanner.close();
+    }   
+}
