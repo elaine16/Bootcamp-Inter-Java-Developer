8ff6a6a7d8c914251c3bf6bcc5e7e0c611d4e687
elaine16 <elaine.adm.redes@gmail.com>
2021-02-24 09:39:30 -0300

    Update ConjuntosBonsOuRuins.java

--- Desafios/Praticando programação em Java/ConjuntosBonsOuRuins.java
+++ Desafios/Praticando programação em Java/ConjuntosBonsOuRuins.java
@@ -30,3 +30,32 @@ Conjunto Bom
  
 */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Desafio2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        while (N != 0) {
+            List<String> conjunto = new ArrayList<>();
+            for (int i = 0; i < N; i++)
+                conjunto.add(br.readLine());
+
+        conjunto = conjunto.stream().sorted().collect(Collectors.toList());
+
+        String saida = "Conjunto Bom";
+        for (int i = 0; i < N - 1; i++)
+            if (conjunto.get(i + 1).startsWith(conjunto.get(i)))
+                saida = "Conjunto Ruim";
+
+        System.out.println(saida);
+        N = Integer.valueOf(br.readLine());
+        }
+    }
+}
