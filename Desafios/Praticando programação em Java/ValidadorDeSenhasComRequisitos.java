f6d0c7c04eec595e3edaa5498d2ae864fddb6f19
elaine16 <elaine.adm.redes@gmail.com>
2021-02-24 09:44:10 -0300

    Update ValidadorDeSenhasComRequisitos.java

--- Desafios/Praticando programação em Java/ValidadorDeSenhasComRequisitos.java
+++ Desafios/Praticando programação em Java/ValidadorDeSenhasComRequisitos.java
@@ -43,3 +43,28 @@ Senha invalida.
 Senha valida.
  
 */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+
+public class Desafio5 {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        String senha;
+        final String requeridos = "^(?:(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).*)$";
+        final String permitidos = "[a-zA-Z0-9]+";
+        
+        while(scr.hasNextLine()) {
+            senha = scr.nextLine();
+            
+            if(senha.length() >=6 && senha.length() <= 32 && senha.matches(permitidos) && senha.matches(requeridos)){
+                System.out.println("Senha valida.");
+            } else {
+                System.out.println("Senha invalida.");
+            }
+        }
+    }
+}
